994602e3c2e57cabaeadcfad3e84f2fc847cfc95
Simon Hagos <Simon10Hagos50@gmail.com>
2022-10-10 17:19:27 -0500

    Project code

---
+++ src/Credentials.js
@@ -0,0 +1,8 @@
+const Credentials = () => {
+    return {
+        ClientId: 'ff25f23238ed4115a458cb5e76b0b2a9',
+        ClientSecret: '84eb98a159e34deb9b471ad61fc88f2b'
+    }
+}
+
+export { Credentials };
